4c45627f47596d9f030a4e278caf9b168295501f
SaQie <Kamcio99@interia.com>
2026-03-29 11:20:59 +0200

    added exception handler for project and environment

---
+++ src/main/java/pl/feature/toggle/service/configuration/environment/infrastructure/in/rest/EnvironmentExceptionHandler.java
@@ -0,0 +1,82 @@
+package pl.feature.toggle.service.configuration.environment.infrastructure.in.rest;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.feature.toggle.service.configuration.environment.domain.exception.*;
+import pl.feature.toggle.service.web.ErrorCode;
+import pl.feature.toggle.service.web.ErrorResponse;
+import pl.feature.toggle.service.web.correlation.CorrelationProvider;
+
+import static pl.feature.toggle.service.web.ErrorCode.*;
+
+@ControllerAdvice
+@AllArgsConstructor
+class EnvironmentExceptionHandler {
+
+    private final CorrelationProvider correlationProvider;
+
+    @ExceptionHandler(EnvironmentUpdateFailedException.class)
+    ResponseEntity<ErrorResponse> handle(EnvironmentUpdateFailedException exception) {
+        var errorResponse = createErrorResponse(ENVIRONMENT_WAS_MODIFIED_BY_ANOTHER_REQUEST, exception);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(EnvironmentNotFoundException.class)
+    ResponseEntity<ErrorResponse> handle(EnvironmentNotFoundException exception) {
+        var errorResponse = createErrorResponse(ENVIRONMENT_NOT_FOUND, exception);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(EnvironmentNotAssignedToProjectException.class)
+    ResponseEntity<ErrorResponse> handle(EnvironmentNotAssignedToProjectException exception) {
+        var errorResponse = createErrorResponse(ENVIRONMENT_NOT_ASSIGNED_TO_PROJECT, exception);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(EnvironmentAlreadyExistsException.class)
+    ResponseEntity<ErrorResponse> handle(EnvironmentAlreadyExistsException exception) {
+        var errorResponse = createErrorResponse(ENVIRONMENT_ALREADY_EXISTS, exception);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(CannotOperateOnArchivedEnvironmentException.class)
+    ResponseEntity<ErrorResponse> handle(CannotOperateOnArchivedEnvironmentException exception) {
+        var errorResponse = createErrorResponse(ENVIRONMENT_ARCHIVED, exception);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(CannotOperateOnEnvironmentForArchivedProjectException.class)
+    ResponseEntity<ErrorResponse> handle(CannotOperateOnEnvironmentForArchivedProjectException exception) {
+        var errorResponse = createErrorResponse(PROJECT_ARCHIVED, exception);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(CannotCreateEnvironmentForMissingProjectException.class)
+    ResponseEntity<ErrorResponse> handle(CannotCreateEnvironmentForMissingProjectException exception) {
+        var errorResponse = createErrorResponse(PROJECT_NOT_FOUND, exception);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+
+    private ErrorResponse createErrorResponse(ErrorCode errorCode, Exception e) {
+        return ErrorResponse.from(errorCode, e, correlationProvider.current());
+    }
+
+}
